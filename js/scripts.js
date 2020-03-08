b594d20635b900ee4dabcd450dfeb6502b46fbd3
Ross Beckman <aguywithgames1212@gmail.com>
2020-03-08 14:45:59 -0700

    create numWords function.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//back-end logic
+$(document).ready(function() {
+  $("form#numWords").submit(function(event) {
+    event.preventDefault();
+    var inputNum = $("#converter").val()
+    var one = 1;
+    var two = 2;
+    var three = 
+    var result = inputNum
+    
+    }
+  });
+});
