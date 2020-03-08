aee0a0d4b806d2b89d6666077c2fd1260ec42a2d
Ross Beckman <aguywithgames1212@gmail.com>
2020-03-08 16:52:47 -0700

    create mrBooper function

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,34 @@
-//back-end logic
-$(document).ready(function() {
-  $("form#numWords").submit(function(event) {
-    event.preventDefault();
-    var inputNum = $("#converter").val()
-    var one = 1;
-    var two = 2;
-    var three = 
-    var result = inputNum
-    
+//back-end
+function mrBooper(number) {
+  var resultArray = [];
+  for (count = 0; count < number +1; count++) {
+    var inputString = count.toString();
+    if (inputString.includes("3")) {
+      resultArray.push("<li>Won't you be my neighbor?</li>");
+    } else if (inputString.includes("2")) {
+      resultArray.push("<li>Boop</li>");
+    } else if (inputString.includes("1")) {
+      resultArray.push("<li>Beep</li>");
+    } else {
+      resultArray.push("<li>" + inputString + "</li>");
     }
+  }
+  return resultArray;
+}
+
+
+
+
+
+//front-end
+$(document).ready(function() {
+  $("#numWords").submit(function(event) {
+    event.preventDefault();
+    $("#result").text("");
+    var inputNum = parseInt($("#converter").val());
+    var mrBooperArray = mrBooper(inputNum);
+    mrBooperArray.forEach(function(item){
+      $("#result").append(item);
+    }) 
   });
 });
